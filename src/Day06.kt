139b86a88406d6de4a205a8d55bf6ad57fcd5aa4
Kent@lhind.hp.g5 <kent.yuan@Gmail.com>
2021-12-06 11:33:18 +0100

    day06

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,14 +1,33 @@
+//https://adventofcode.com/2021/day/6
+
 fun main() {
     val input = readInput("Day06")
 
     fun part1(input: List<String>): Int {
-        return 0
+        var myList = input[0].split(",").map { it.toInt() }
+        repeat(80) { _ ->
+            val zeros = myList.count { it == 0 }
+            myList = myList.map { if (it - 1 >= 0) it - 1 else 6 }.toMutableList()
+                .also { it.addAll(MutableList(zeros) { 8 }) }
+        }
+        return myList.size
     }
 
-    fun part2(input: List<String>): Int {
-        return 0
+    fun part2(input: List<String>): Long { //Long required
+        var genMap =
+            input[0].split(",").map { it.toInt() }.groupBy { it }.mapValues { (_, v) -> v.count().toLong() }
+                .toMutableMap()
+        repeat(256) {
+            genMap = genMap.mapKeys { (k, _) -> k - 1 }.toMutableMap().also {
+                it[8] = it.getOrDefault(-1, 0)
+                it[6] = it.getOrDefault(-1, 0) + it.getOrDefault(6, 0)
+                it.remove(-1)
+            }
+        }
+        return genMap.values.sum()
     }
 
     println("part1: ${part1(input)}")
     println("part2: ${part2(input)}")
 }
+
---
+++ src/Day07.kt
@@ -0,0 +1,16 @@
+
+fun main() {
+    val input = readInput("Day07")
+
+
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    println("part1: ${part1(input)}")
+    println("part2: ${part2(input)}")
+}
