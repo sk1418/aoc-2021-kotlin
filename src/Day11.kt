0a16f225df2f335b17a34b6c567eba00ff8fe81e
Kent@lhind.hp.g5 <kent.yuan@Gmail.com>
2021-12-11 19:58:07 +0100

    day11

--- src/Day11.kt
+++ src/Day11.kt
@@ -6,22 +6,64 @@ fun main() {
     val testInput = readTestInput(today)
 
     fun part1(input: List<String>): Long {
-        return 0
+        val oMatrix = OctopusMatrix(input)
+        repeat(100) {
+            oMatrix.take1Step()
+        }
+        return oMatrix.flashTimes
     }
 
     fun part2(input: List<String>): Long {
-        return 0
+        val oMatrix = OctopusMatrix(input)
+        var step = 0L
+        while (!oMatrix.allFlashed()) {
+            step++
+            oMatrix.take1Step()
+        }
+        return step
     }
 
     part1(testInput).also {
         println("TEST part1: $it")
-        check(it == 0L)
+        check(it == 1656L)
     }
     println("part1: ${part1(input)}")
 
     part2(testInput).also {
         println("TEST part2: $it")
-        check(it == 0L)
+        check(it == 195L)
     }
     println("part2: ${part2(input)}")
+}
+
+data class OctopusMatrix(val input: List<String>) {
+    private val matrix: List<MutableList<Int>> = input.map { it.map { c -> c.digitToInt() }.toMutableList() }
+    private val maxI = matrix.size - 1
+    private val maxJ = matrix[0].size - 1
+    var flashTimes = 0L
+
+    //extension functions
+    private fun Pair<Int, Int>.outOfBound() = first < 0 || first > maxI || second < 0 || second > maxJ
+
+    fun allFlashed() = matrix.all { r -> r.all { it == 0 } }
+
+    fun take1Step() {
+        (0..maxI).forEach { i -> (0..maxJ).forEach { j -> matrix[i][j]++ } }
+        matrix.forEachIndexed { i, row -> row.forEachIndexed { j, v -> if (v > 9) flash(i, j) } }
+    }
+
+    private fun flash(i: Int, j: Int) {
+        flashTimes++
+        matrix[i][j] = 0
+        findAdjacent(i, j).forEach { (i, j) ->
+            if (matrix[i][j] != 0) {
+                matrix[i][j]++.takeIf { matrix[i][j] > 9 }?.apply { flash(i, j) }
+            }
+        }
+    }
+
+    private fun findAdjacent(i: Int, j: Int): List<Pair<Int, Int>> = setOf(
+        i to j - 1, i to j + 1, i - 1 to j, i + 1 to j, //L,R,U,D
+        i - 1 to j - 1, i - 1 to j + 1, i + 1 to j + 1, i + 1 to j - 1 // diagonal(4)
+    ).filterNot { it.outOfBound() }
 }
---
+++ src/Day12.kt
@@ -0,0 +1,27 @@
+// https://adventofcode.com/2021/day/12
+fun main() {
+    val today = "Day12"
+
+    val input = readInput(today)
+    val testInput = readTestInput(today)
+
+    fun part1(input: List<String>): Long {
+        return 0
+    }
+
+    fun part2(input: List<String>): Long {
+        return 0
+    }
+
+    part1(testInput).also {
+        println("TEST part1: $it")
+        check(it == 0L)
+    }
+    println("part1: ${part1(input)}")
+
+    part2(testInput).also {
+        println("TEST part2: $it")
+        check(it == 0L)
+    }
+    println("part2: ${part2(input)}")
+}
